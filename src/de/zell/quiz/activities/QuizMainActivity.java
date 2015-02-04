fde3374376ac7c875ed8c9f9b4ee90ed9ce48559
zell.christopher <zelldon91@gmail.com>
2015-02-04 23:15:48 +0100

    added gpl3 license header to the files

--- src/de/zell/quiz/activities/QuizMainActivity.java
+++ src/de/zell/quiz/activities/QuizMainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Christopher Zell <zelldon91@googlemail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.zell.quiz.activities;
 
 import android.os.Bundle;
@@ -6,18 +22,13 @@ import de.zell.android.util.PropertiesProvider;
 import de.zell.android.util.activities.MainNavigationActivity;
 import de.zell.android.util.json.JSONUnmarshaller;
 import de.zell.quiz.activities.fragments.QuizViewPagerFragment;
-import de.zell.quiz.entities.Answer;
-import de.zell.quiz.entities.Quiz;
 import de.zell.quiz.entities.QuizList;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
